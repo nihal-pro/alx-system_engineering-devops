1a4e6423cf6542af56fe479513ffea5ef9bd79ad
NIHAL <nihaleloudiki16@gmail.com>
2024-05-23 21:58:39 +0100

    mysql

--- 0x14-mysql/mysql_setup.sql
+++ 0x14-mysql/mysql_setup.sql
@@ -1,8 +1,14 @@
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 SHOW GRANTS FOR 'holberton_user'@'localhost';
-CREATE DATABASE tyrell_corp;
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
-CREATE TABLE nexus6 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS nexus6 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
 INSERT INTO nexus6 (name) VALUES ('Leon');
 GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
+CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'replica_user';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+~                                                                                                                                      
+~                                                                                                                                      
+~                        
