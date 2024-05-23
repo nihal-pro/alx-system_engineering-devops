dcd0b94ae5b345df5a876c7456a99719d604f6b5
NIHAL <nihaleloudiki16@gmail.com>
2024-05-23 19:06:21 +0100

    mysql

--- 0x14-mysql/mysql_setup.sql
+++ 0x14-mysql/mysql_setup.sql
@@ -1,3 +1,8 @@
 CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 SHOW GRANTS FOR 'holberton_user'@'localhost';
+CREATE DATABASE tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
+INSERT INTO nexus6 (name) VALUES ('Leon');
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
