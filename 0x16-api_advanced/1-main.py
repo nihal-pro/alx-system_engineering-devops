3b9e3868564ab0c7d95fd316d7bfe35733f550f4
NIHAL <nihaleloudiki16@gmail.com>
2024-06-05 00:03:21 +0100

    API ad

--- 0x16-api_advanced/1-main.py
+++ 0x16-api_advanced/1-main.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == '__main__':
     top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+        print("Please pass an argument for the subreddit to search")
     else:
         top_ten(sys.argv[1])
