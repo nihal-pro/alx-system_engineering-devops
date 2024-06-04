1e44b23d2f1bc0acb6402121e45dfdd195bd9c82
NIHAL <nihaleloudiki16@gmail.com>
2024-06-04 23:34:20 +0100

    API ad

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
-returns the number of subscribers"""
+"""
+this doc for module
+"""
 import requests
+
+header = {
+    "User-Agent": "MyCustomUserAgent/1.0"
+}
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """method doc"""
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        response = requests.get(url, allow_redirects=False, headers=header)
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    except Exception:
+        return 0
